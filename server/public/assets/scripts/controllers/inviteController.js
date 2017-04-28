fb7665ff41a3ca319263b86fb2356a96ed146b0e
Teigen Leonard <teigenl@gmail.com>
2017-04-27 19:10:31 -0500

    nodemailer is working!

--- server/public/assets/scripts/controllers/inviteController.js
+++ server/public/assets/scripts/controllers/inviteController.js
@@ -2,6 +2,11 @@ myApp.controller('InviteController', [ 'MailService', function(MailService) {
   var invite = this;
   console.log('InviteController');
 
-  invite.emailInvite = MailService.emailInvite;
+  invite.submitForm = MailService.emailInvite;
 
+  // invite.submitForm = function(info) {
+  //     MailService.emailInvite(info);
+  //     console.log('Info@submitForm: ', info);
+  //
+  // };
 }]);
--- server/public/assets/scripts/factory/MailService.js
+++ server/public/assets/scripts/factory/MailService.js
@@ -1,7 +1,18 @@
 myApp.factory( 'MailService', [ '$http', function($http){
-  var emailInvite = 'Inside MailService';
+  var hit = 'Inside MailService';
+
+
+
+  var emailInvite = function(info) {
+    console.log('line 7', info);
+    $http.post( '/mail' , info ).then(function(response){
+      console.log( 'Email sent: ', response.data );
+    });
+  };
+
 
   return {
+    hit : hit,
     emailInvite : emailInvite
   };
 }]);
