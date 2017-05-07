0f9cf4e7d518b8b73e6811d2ef515dccf5b106af
Teigen Leonard <teigenl@gmail.com>
2017-05-07 15:19:16 -0500

    added minimal bootstrap to clarify divs in items.html

--- server/public/assets/scripts/controllers/packingListController.js
+++ server/public/assets/scripts/controllers/packingListController.js
@@ -1,23 +1,21 @@
-myApp.controller('PackingListController', ['PackingService','LoginService', function(PackingService, LoginService) {
-  var list = this;
-  console.log('packingListController');
+myApp.controller('PackingListController', ['PackingService', 'LoginService', function(PackingService, LoginService) {
+    var list = this;
+    // console.log('packingListController');
+    list.postItem = PackingService.postItem;
+    console.log('newItem when controller loads: ', list.newItem);
 
-  list.postItem = PackingService.postItem;
-  console.log('newItem when controller loads: ', list.newItem);
+    list.itemsObject = PackingService.itemsObject;
+    console.log(list.itemsObject);
+    console.log('here lies user id: ', LoginService.user.user_id);
+    // console.log("Here you go: ", PackingService.selectedTrip);
 
-  list.itemsObject = PackingService.itemsObject;
-  console.log(list.itemsObject);
-  console.log('here lies user id: ', LoginService.user.user_id);
-
-  console.log("Here you go: ", PackingService.selectedTrip);
-
-  // if(PackingService.selectedTrip.id !== 999){
+    if(PackingService.selectedTrip.id !== 999){
     PackingService.getItems(PackingService.selectedTrip.id);
-  // }
-  console.log('clearItem?');
-  list.newItem = {
-      name : '',
-      quantity : '',
-      trip_id : PackingService.selectedTrip.id,
+    }
+    console.log('clearItem?');
+    list.newItem = {
+        name: '',
+        quantity: '',
+        trip_id: PackingService.selectedTrip.id,
     };
 }]);
